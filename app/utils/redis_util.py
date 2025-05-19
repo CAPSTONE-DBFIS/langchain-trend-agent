fdc9b36ee768f24323f4d589d98ef75fcb3a864c
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2025-05-19 18:18:43 +0900

    feature/team-file-rag-chatbot: 주석 변경

--- app/utils/redis_util.py
+++ app/utils/redis_util.py
@@ -5,7 +5,7 @@ import redis
 load_dotenv()
 
 def get_redis_client():
-    """Redis 클라이언트 연결."""
+    """Redis 클라이언트 연결하는 함수"""
     required_vars = ["REDIS_HOST", "REDIS_PORT", "REDIS_PASSWORD"]
     for var in required_vars:
         if not os.getenv(var):
@@ -28,9 +28,7 @@ def get_redis_client():
         raise ValueError(f"Redis 연결 중 오류: {str(e)}")
 
 def clear_all_cache_db():
-    """
-    현재 선택된 Redis 데이터베이스의 모든 키를 삭제합니다.
-    """
+    """현재 선택된 Redis 데이터베이스의 모든 키를 삭제하는 함수"""
     r = get_redis_client()
     r.flushdb()
     print("현재 Redis DB의 모든 키를 삭제했습니다.")
